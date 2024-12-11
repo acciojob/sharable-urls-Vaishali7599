de1e9e296552a3510d2fd72ebc23b549ec443d06
Vaishali7599 <140495282+Vaishali7599@users.noreply.github.com>
2024-12-11 08:03:27 +0000

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// your code here
+let btn = document.querySelector("#button");
+
+btn.addEventListener("click", (e)=>{
+    e.preventDefault;
+    let s_name = document.querySelector("#name").value;
+    let s_year = document.querySelector("#year").value;
+
+    let queryPara = new URLSearchParams();
+    queryPara.set("name", s_name);
+    queryPara.set("year", s_year);
+
+    let url = document.querySelector("#url");
+
+    url.textContent += queryPara.toString();
+
+
+})
