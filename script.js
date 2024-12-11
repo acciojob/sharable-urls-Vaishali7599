eae68507c086f91b8e1340752e3b067c94d018d9
Vaishali7599 <140495282+Vaishali7599@users.noreply.github.com>
2024-12-11 13:36:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-let btn = document.querySelector("#button");
+let s_btn = document.querySelector("button");
 
-btn.addEventListener("click", (e)=>{
-    e.preventDefault;
+s_btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    
     let s_name = document.querySelector("#name").value;
     let s_year = document.querySelector("#year").value;
 
+    // console.log(url);
     let queryPara = new URLSearchParams();
-    queryPara.set("name", s_name);
-    queryPara.set("year", s_year);
-
-    let url = document.querySelector("#url");
+    queryPara.set('name', s_name);
+    queryPara.set('year', s_year);
+    
+    let url = document.getElementById("url");
+    
+    // console.log(queryPara);
 
     url.textContent += queryPara.toString();
-
-
 })
+
